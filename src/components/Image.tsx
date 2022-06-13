8690ffbc18c5e19119adf966eca3865a9499c597
YuTaExtend <yuta.extend@gmail.com>
2022-06-13 18:33:39 +0900

    image

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,7 +1,7 @@
 import NextImage from "next/image";
 
-const customLoader = ({ src, width }: any) => {
-  return `${src}?w=${width}`;
+const customLoader = ({ src, width, quality }: any) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
 };
 const Image = ({ ...props }: any) => {
   return <NextImage {...props} loader={customLoader} />;
